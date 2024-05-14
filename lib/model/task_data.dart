6bf3c1508e47da9ceded1d08a675b1f79b54af5f
Rajit Deb <rajitdeb456@gmail.com>
2024-05-14 15:32:26 +0530

    Removed sample tasks and made the list completely empty for starting fresh

--- lib/model/task_data.dart
+++ lib/model/task_data.dart
@@ -5,40 +5,7 @@ import 'package:todoey_flutter/util/immutable_list.dart';
 class TaskData extends ChangeNotifier {
   /// Made tasks private so that we can't modify it directly
   /// Rather use the getter and setter methods to do so
-  final List<Task> _tasks = [
-    Task(
-      taskDescription: "Buy Milk",
-      isComplete: false,
-    ),
-    Task(
-      taskDescription: "Buy Eggs",
-      isComplete: false,
-    ),
-    Task(
-      taskDescription: "Buy Detergent",
-      isComplete: true,
-    ),
-    Task(
-      taskDescription: "Buy Sleep Medicine",
-      isComplete: false,
-    ),
-    Task(
-      taskDescription: "Buy Revolving Chair for Office",
-      isComplete: false,
-    ),
-    Task(
-      taskDescription: "Go to Post Office",
-      isComplete: false,
-    ),
-    Task(
-      taskDescription: "Meet Aditi at 24x7 Cafe",
-      isComplete: false,
-    ),
-    Task(
-      taskDescription: "Attend Office Meeting @ 10:30AM",
-      isComplete: false,
-    ),
-  ];
+  final List<Task> _tasks = [];
 
   /// `ImmutableList<T>` is our custom list data structure
   /// to get rid of the .add() method problem
